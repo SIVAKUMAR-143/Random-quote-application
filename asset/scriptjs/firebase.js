236093f2dc394fc68222a8305caa9540bc75e8b8
SIVAKUMAR-143 <sivakumar42003z@gamil.com>
2022-04-11 19:21:51 +0530

    s

---
+++ asset/scriptjs/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBE6It5IRmuVnUpyGf2fI8NpvhK71YR4DY",
+  authDomain: "my-quote-application.firebaseapp.com",
+  projectId: "my-quote-application",
+  storageBucket: "my-quote-application.appspot.com",
+  messagingSenderId: "422952261738",
+  appId: "1:422952261738:web:e5fdc425dcc4fef4bad213"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
